8ce25d3419346fad447db9c4e87967c5e49acd33
Foucsi <julien.foucartreact@gmail.com>
2022-12-30 21:45:10 +0100

    commit

--- routes/tweets.js
+++ routes/tweets.js
@@ -13,7 +13,7 @@ router.post("/addTweets/:username", (req, res) => {
   });
 });
 
-/*Retire un tweet avec le parametre username */
+/*Supprime un tweet avec le parametre username */
 router.delete("/removeTweets/:username", (req, res) => {
   User.findOneAndUpdate(
     { username: req.params.username },
@@ -28,7 +28,32 @@ router.delete("/removeTweets/:username", (req, res) => {
 /* Recupere tous les tweet d'un utilisateur avec parametre username */
 router.get("/allTweets/:username", (req, res) => {
   User.findOne({ username: req.params.username }).then((data) => {
-    res.json({ result: true, data: data.tweets });
+    if (data) {
+      res.json({ result: true, data: data.tweets });
+    } else {
+      res.json({ result: false, data: data.tweets });
+    }
+  });
+});
+
+/* Récupere tout les tweets de tous les utilisateurs */
+router.get("/allTweets", (req, res) => {
+  User.find().then((data) => {
+    res.json({ result: true, data: data.map((e) => e.tweets) });
+  });
+});
+
+/* Récupére le nom du user en fonction du tweet */
+router.get("/userByTweet/:tweet", (req, res) => {
+  User.find(
+    { tweets: { $elemMatch: { tweet: req.params.tweet } } },
+    { username: 1 }
+  ).then((data) => {
+    if (data.length > 0) {
+      res.json({ result: true, data: data[0].username });
+    } else {
+      res.json({ result: false });
+    }
   });
 });
 
