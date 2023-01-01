5b094199fc479a0e2a5598b230e7a80de02f882d
Foucsi <julien.foucartreact@gmail.com>
2023-01-01 23:08:41 +0100

    commit

--- routes/tweets.js
+++ routes/tweets.js
@@ -18,8 +18,9 @@ router.post("/isLiked/:tweet", (req, res) => {
   User.findOneAndUpdate(
     // Cherchez l'utilisateur qui a posté le tweet
     { "tweets.tweet": req.params.tweet },
-    // Ajoutez le like à la liste de likes du tweet
-    { $push: { "tweets.$.isliked": { liked: req.body.liked } } },
+
+    //L'opérateur $set est utilisé pour remplacer
+    { $set: { "tweets.$.isliked": { liked: req.body.liked } } },
     // Retournez le document modifié au lieu du document d'origine
     { returnOriginal: false }
   ).then((data) => {
@@ -28,13 +29,42 @@ router.post("/isLiked/:tweet", (req, res) => {
       // Trouvez le tweet dans la liste de tweets de l'utilisateur
       const tweet = data.tweets.find((e) => e.tweet === req.params.tweet);
       // Renvoyez le tweet avec les données mises à jour
-      res.json({ result: true, data: tweet });
+
+      res.json({ result: true, data: tweet.isliked[0].liked });
     } else {
       // Gérez l'erreur ici
       res.json({ result: false });
     }
   });
 });
+
+/* recuperer si le like et true ou false */
+router.get("/getLiked/:tweet", (req, res) => {
+  User.findOne({ "tweets.tweet": req.params.tweet }).then((data) => {
+    res.json({
+      result: true,
+      data: data.tweets.map((e) => e.isliked[0]),
+      // data: data.map((elmt) => elmt.tweets[0].isliked[0]),
+    });
+  });
+});
+
+// router.get("/getTweetId/:tweet", (req, res) => {
+//   User.findOne({ "tweets.tweet": req.params.tweet }, (err, user) => {
+//     if (err) {
+//       // Gérez l'erreur ici
+//       res.status(500).json({ error: err });
+//     } else {
+//       // Récupérez l'identifiant du tweet
+//       const tweetId = user.tweets.find(
+//         (tweet) => tweet.tweet === req.params.tweet
+//       )._id;
+
+//       // Renvoyez l'identifiant dans la réponse
+//       res.json({ tweetId: tweetId });
+//     }
+//   });
+// });
 
 /*Supprime un tweet avec le parametre username */
 router.delete("/removeTweets/:username", (req, res) => {
@@ -50,14 +80,16 @@ router.delete("/removeTweets/:username", (req, res) => {
 
 /* Recupere tous les tweet d'un utilisateur avec parametre username */
 router.get("/allTweets/:username", (req, res) => {
-  User.findOne({ username: req.params.username }).then((data) => {
-    if (data) {
+  User.findOne({ username: req.params.username })
+    .then((data) => {
       res.json({ result: true, data: data.tweets });
-    } else {
-      res.json({ result: false, data: data.tweets });
-    }
-  });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err });
+    });
 });
+
+/*TEST !!! recupere tous les tweets en fonction du usernmae  */
 
 /*Récupere le nombre de tweet par user */
 router.get("/numberTweet/:username", (req, res) => {
@@ -70,9 +102,13 @@ router.get("/numberTweet/:username", (req, res) => {
 
 /* Récupere tout les tweets de tous les utilisateurs */
 router.get("/allTweets", (req, res) => {
-  User.find().then((data) => {
-    res.json({ result: true, data: data.map((e) => e.tweets) });
-  });
+  User.find()
+    .then((data) => {
+      res.json({ result: true, data: data.map((e) => e.tweets) });
+    })
+    .catch((err) => {
+      res.status(500).json({ result: false, error: err });
+    });
 });
 
 /* Récupére le nom du user en fonction du tweet */
